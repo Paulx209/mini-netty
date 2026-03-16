e18787b6a4df6b534d3c3836bf2f0a5af97f1d32
get <249628024@qq.com>
2026-03-16 20:44:55 +0800

    test(9.single-thread-event-loop):单线程事件循环实现

---
+++ src/test/java/com/getian/netty/channel/EventLoopInterfaceTest.java
@@ -0,0 +1,13 @@
+package com.getian.netty.channel;
+
+/**
+ * EventLoop 和 EventLoopGroup 接口定义测试
+ *
+ *  验证接口是否正确定义了所有必需的方法。
+ * @Author: sonicge
+ * @CreateTime: 2026-03-15
+ */
+
+public class EventLoopInterfaceTest {
+
+}
